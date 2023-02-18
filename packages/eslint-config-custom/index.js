605990e8097a3ab67141f381f1a164cf8ac0be0d
Cristian Romero <cristian.romero@vtex.com.br>
2023-02-18 03:00:58 -0500

    Git config eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,10 @@
 module.exports = {
   extends: ["next", "turbo", "prettier"],
   rules: {
+    /** Rules of react */
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+
+    /** Rules generals */
   },
 };
