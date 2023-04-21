305411a37ee52696beee9868c9956ff2043b605f
crisak <opensource1998@gmail.com>
2023-04-21 01:50:49 -0500

    Configure Cognito authentication by default

--- packages/blito-models/aws-exports.js
+++ packages/blito-models/aws-exports.js
@@ -5,7 +5,7 @@ const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_appsync_graphqlEndpoint": "https://gzw7npcnxzbbfidiv6izaqig2u.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
-    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
     "aws_appsync_apiKey": "da2-ue5gzet7b5eklmne2krjnb3dxe",
     "aws_cognito_identity_pool_id": "us-east-1:28adc326-42d2-4e2f-98d7-50390a268036",
     "aws_cognito_region": "us-east-1",
