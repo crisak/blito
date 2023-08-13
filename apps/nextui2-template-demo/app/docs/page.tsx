a286da34024792f4a09cd23991ed21b194ad48c9
crisak <opensource1998@gmail.com>
2023-08-12 23:50:28 -0500

    Update dependencies

--- apps/nextui2-template-demo/app/docs/page.tsx
+++ apps/nextui2-template-demo/app/docs/page.tsx
@@ -1,9 +1,11 @@
 import { title } from "@/components/primitives";
+import { Button } from "@nextui-org/button";
 
 export default function DocsPage() {
 	return (
 		<div>
 			<h1 className={title()}>Docs</h1>
+			<Button variant="bordered" color="primary">Holi</Button>
 		</div>
 	);
 }
